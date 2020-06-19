6cfb216b637a77599a4b554b7e8b33ab6fa84166
Juan Pablo <jpablomgil@gmail.com>
2020-06-19 10:38:29 -0500

    Setup linterns

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './containers/App';
 
-
 ReactDOM.render(
-   <App />,
-  document.getElementById('root')
+  <App />,
+  document.getElementById('root'),
 );
-

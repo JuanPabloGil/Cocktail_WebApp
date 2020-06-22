8d861933e015d6ef0189dbead923a5cda6a0487a
Juan Pablo <jpablomgil@gmail.com>
2020-06-22 17:25:19 -0500

    Add the store and route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import Router from './components/Router';
+import allReducers from './reducers';
 import './styles/index.css';
-import App from './containers/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+const store = createStore(
+  allReducers,
+  applyMiddleware(thunk),
+  // // eslint-disable-next-line no-underscore-dangle
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router />
+  </Provider>,
   document.getElementById('root'),
 );
